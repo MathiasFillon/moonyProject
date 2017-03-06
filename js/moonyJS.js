8821bafa97fb2f8d41025ee6b77022fbf91b3f03
Sacha GRASSO <gr.sacha.glgm@gmail.com>
2017-03-06 11:55:07 +0100

    ajout slidersJS + créations dossiers

--- js/moonyJS.js
+++ js/moonyJS.js
@@ -1,11 +1,15 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function myFunction1() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function myFunction2() {
+  document.getElementById("mySettings").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     for (let i = 0; i < dropdowns.length; i++) {
